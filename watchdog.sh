bb899f2fc5c6c9a36fdc26cc2af2ea2da52c70c2
tmuskal <tmuskal@gmail.com>
2018-07-08 17:43:33 -0700

    Create watchdog.sh

---
+++ watchdog.sh
@@ -0,0 +1,48 @@
+#! /bin/bash
+PRODUCERACCT=yourbp
+PASS=PWyourpasswordyourpasswordyourpasswordyourpassword
+
+LAST_UNPAID_BLOCKS=0
+LAST_DATE=`date +%s`
+GRACE=252
+INTERVAL=5
+SCHEDULE_GRACE=$GRACE
+API=http://nodes.get-scatter.com
+
+while true;
+do
+        UNPAID_BLOCKS=`curl "$API/v1/chain/get_table_rows" --stderr /dev/null  --data-binary '{"json":true,"scope":"eosio","code":"eosio","table":"producers","limit":5000}' | jq -e '.rows' | jq -c 'sort_by(-(.total_votes|tonumber))' | jq '[limit(21;.[])]' | jq --arg prd "$PRODUCERACCT" '.[] | select(.owner == $prd)' | jq -r '.unpaid_blocks'`
+        if [ "$UNPAID_BLOCKS" == "" ]
+        then
+                echo "not in top 21. sleeping"
+                sleep $SCHEDULE_GRACE
+                LAST_DATE=`date +%s`
+                continue
+        fi
+        if [ "$UNPAID_BLOCKS" != "$LAST_UNPAID_BLOCKS" ]
+        then
+                if [ "$LAST_UNPAID_BLOCKS" != "0" ]
+                then
+                        echo "producing"
+                else
+                        echo "init"
+                fi
+                LAST_UNPAID_BLOCKS=$UNPAID_BLOCKS
+                LAST_DATE=`date +%s`
+                sleep 1
+        else
+                let "DELTA=`date +%s`-$LAST_DATE"
+                if [ "$DELTA" -gt "$GRACE" ]
+                then
+                        cleos wallet unlock --password $PASS -n watchdog  > /dev/null 2> /dev/null
+                        echo "NOT producing for too long - unregistering!!!!!"
+                        # notify slack/phone/etc.
+                        cleos -u $API system unregprod $PRODUCERACCT -p $PRODUCERACCT@watchdog
+                        cleos wallet lock -n watchdog > /dev/null 2> /dev/null
+                        sleep $SCHEDULE_GRACE
+                else
+                        echo "not currently producing"
+                fi
+                sleep $INTERVAL
+        fi
+done
